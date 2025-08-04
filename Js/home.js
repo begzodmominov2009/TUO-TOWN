0b1153cfd2b9385725a8ca8fa882244c8d392387
begzodmominov2009 <begzodmominov32@gmail.com>
2025-08-04 05:31:23 +0300

    adding toggle

---
+++ Js/home.js
@@ -0,0 +1,63 @@
+const MenuToggle = document.getElementById("Toggle");
+const menu_bar = document.getElementById("menu_bar")
+const close = document.getElementById("close")
+
+
+MenuToggle.addEventListener("click" , () => {
+    menu_bar.classList.add("menus")
+})
+close.addEventListener("click" , () => {
+    menu_bar.classList.remove("menus")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 1,
+        },
+        1000: {
+            items: 1,
+        },
+    },
+});
