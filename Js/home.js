f4ac2fccdaf5b8cd5b51099d21f30240777cdb9d
begzodmominov2009 <begzodmominov32@gmail.com>
2025-08-04 13:52:31 +0300

    adding

--- Js/home.js
+++ Js/home.js
@@ -40,19 +40,3 @@ $(".owl-carousel").owlCarousel({
         },
     },
 });
-$('.owl-none').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 1
-        },
-        1000: {
-            items: 1
-        }
-    }
-})
